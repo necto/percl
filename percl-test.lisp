f43ba479731886e9d7e35c8f344b3d8572655646
Necto <necto.ne@gmail.com>
2012-03-03 19:14:27 +0400

    added :enum slot type

--- percl-test.lisp
+++ percl-test.lisp
@@ -7,7 +7,7 @@
    ((sl1 :initarg :sl1)
    (sl2 :initarg :sl2)))
 
-(defclass tst-db (database)
+(defclass tst-db (database-base)
   (cont))
 
 (defmethod initialize-instance ((db tst-db) &key) ;TODO: know more about method sequence
@@ -16,18 +16,21 @@
 
 (defvar *db* (make-instance 'tst-db))
 
-(print (macroexpand-1 '(generate-methods test ('cont tst-db) (('sl1 "sl1") ('sl2 "sl2")))))
-(generate-methods test ('cont tst-db) (('sl1 "sl1") ('sl2 "sl2")))
+(print (macroexpand-1 '(generate-methods test ('cont tst-db) (('sl1 "sl1" :set (('ans 42) ('res 33))) ('sl2 "sl2")))))
+(generate-methods test ('cont tst-db) (('sl1 "sl1" :set (('ans "42") ('res 45))) ('sl2 "sl2")))
 
 
-(let ((ii (load-inst 'test 108 *db*))
-	  (aa (make-instance 'test)))
-  (setf (slot-value aa 'sl1) 42)
-  (setf (slot-value aa 'sl2) 42)
-  (store-inst aa *db*)
+(let ((ii (load-inst 'test 119 *db*)) )
+	(print (slot-value ii 'sl1))
+  (setf (slot-value ii 'sl1) 'ans)
+  (store-inst ii *db*)
+;	  (aa (make-instance 'test)))
+;  (setf (slot-value aa 'sl1) 42)
+;  (setf (slot-value aa 'sl2) 42)
+;  (store-inst aa *db*)
 
-	(print (slot-value ii 'sl2))
-	(print (load-all-instances 'test *db*)))
+	(print (slot-value ii 'sl1)) )
+;	(print (load-all-instances 'test *db*))
 
 (sb-ext:quit)
 
